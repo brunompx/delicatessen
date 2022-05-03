b0c648f38186121cd351ca21c9dc1cef49599d4c
Bruno M Ponsanesi <brunoponsanesi@gmail.com>
2022-05-03 13:54:08 -0300

    updates

--- delicatessen/settings.py
+++ delicatessen/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     'delicatessen/static',
     'dashboard/static',
+    'authentication/static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
